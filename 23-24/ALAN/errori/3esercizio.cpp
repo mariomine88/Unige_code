35f9d4fbabfd0b62a6b163344969201023bcf11f
Eugenio <eugenio.vassallo0103@gmail.com>
2024-10-23 13:56:50 +0200

    finito alan 1 lab

--- 23-24/ALAN/errori/3esercizio.cpp
+++ 23-24/ALAN/errori/3esercizio.cpp
@@ -1,17 +1,6 @@
 #include <iostream>
 #include <cmath>
 
-void find_machine_eps_double(double &eps, int &d) {
-    eps = 1.0;
-    d = 0;
-    while (1.0 + eps > 1.0) {
-        eps /= 2.0;
-        d++;
-    }
-    eps *= 2.0;
-    d -= 1;
-}
-
 void find_machine_eps_float(float &eps, int &d) {
     eps = 1.0f;
     d = 0;
@@ -23,18 +12,30 @@ void find_machine_eps_float(float &eps, int &d) {
     d -= 1;
 }
 
-int main() {
-    double eps_double;
-    int d_double;
-    find_machine_eps_double(eps_double, d_double);
+void find_machine_eps_double(double &eps, int &d) {
+    eps = 1.0;
+    d = 0;
+    while (1.0 + eps > 1.0) {
+        eps /= 2.0;
+        d++;
+    }
+    eps *= 2.0;
+    d -= 1;
+}
 
+int main() {
     float eps_float;
     int d_float;
     find_machine_eps_float(eps_float, d_float);
 
-    std::cout << "Machine epsilon for double precision: " << eps_double << " = 2^-d where d is: " << d_double << std::endl;
+
+    double eps_double;
+    int d_double;
+    find_machine_eps_double(eps_double, d_double);
 
     std::cout << "Machine epsilon for single precision: " << eps_float << " = 2^-d where d is: " << d_float << std::endl;
+
+    std::cout << "Machine epsilon for double precision: " << eps_double << " = 2^-d where d is: " << d_double << std::endl;
 
     return 0;
 }
