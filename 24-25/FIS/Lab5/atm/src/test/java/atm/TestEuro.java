e8dd906e42c64633e2124c87c113154ee58646ba
Eujiin <eugenio.vassallo0103@gmail.com>
2024-12-09 15:51:05 +0100

    test done

--- 24-25/FIS/Lab5/atm/src/test/java/atm/TestEuro.java
+++ 24-25/FIS/Lab5/atm/src/test/java/atm/TestEuro.java
@@ -2,7 +2,6 @@ package atm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -20,9 +19,29 @@ class TestEuro {
 
     @Test
     void testSum() {
-        fail("Not yet implemented");
+        Euro result = euro1.sum(euro2);
+        assertEquals(630.5, result.getValue());
     }
 
-    //TODO: complete the class
-   
+    @Test
+    void testSubtract() {
+        Euro result = euro1.subtract(euro2);
+        assertEquals(430.5, result.getValue());
+    }
+
+    @Test
+    void testEqualTo() {
+        Euro euro3 = new Euro(530.5);
+        assertTrue(euro1.equalTo(euro3));
+    }
+
+    @Test
+    void testLessThan() {
+        assertTrue(euro2.lessThan(euro1));
+    }
+
+    @Test
+    void testPrint() {
+        assertEquals("530.5 euro", euro1.print());
+    }
 }
