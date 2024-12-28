e19c1814660df037a0fd8395bc5f55848d1fde0e
mariomine88 <mariominecraft87@gmail.com>
2024-12-29 00:35:13 +0100

    icona nel likck di codivisione

--- 24-25/SAW/ProgettoSAW/public_html/include/header.php
+++ 24-25/SAW/ProgettoSAW/public_html/include/header.php
@@ -1,6 +1,21 @@
-<title>ProgettoSAW</title>
+<title>Threadit - Share Your Thoughts</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="Threadit - A community-driven discussion platform">
+<meta name="keywords" content="Threadit, forum, discussion, community, posts">
+<meta name="author" content="Eugenio&Mario">
+
+<!-- Open Graph / Social Media Meta Tags -->
+<meta property="og:title" content="Threadit - Share Your Thoughts">
+<meta property="og:description" content="Threadit - A community-driven discussion platform">
+<meta property="og:image" content="https://saw.dibris.unige.it/~s5577783/image/favicon.ico">
+<meta property="og:url" content="https://saw.dibris.unige.it/~s5577783/">
+<meta property="og:type" content="website">
+
+<!-- Favicon -->
+<link rel="icon" type="image/svg+xml" href="../image/favicon.svg">
+<link rel="icon" type="image/png" href="../image/favicon.png">
+<link rel="apple-touch-icon" href="../image/favicon.png">
 
 <!-- jQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
@@ -13,5 +28,12 @@
 <!-- Custom CSS -->
 <link rel="stylesheet" href="../css/styles.css">
 
+<!-- Common JS Libraries -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
+
 <!-- Bootstrap Bundle (includes Popper) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+<!-- Global site utilities -->
+<script src="../js/utils.js"></script>
